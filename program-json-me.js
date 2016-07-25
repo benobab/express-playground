1d6e3480ec25086d9f6bf9449e401f5561977f0a
Benobab <benlacroix83@gmail.com>
2016-07-25 10:58:59 +0000

    read file express send to json add error handling

--- program-json-me.js
+++ program-json-me.js
@@ -8,6 +8,10 @@ var app = express();
 
 app.get('*',function(req,res){
     fs.readFile(file,function(err,data){
+        if(err){
+            console.log(err);
+            res.end();
+        }
         res.json(JSON.parse(data));    
     });
 });
